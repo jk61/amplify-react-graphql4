8259be53beddcd0444071ae7627d6fa180b95ce4
Jan Kolbusz <jankolbusz@Jans-MacBook-Pro-3.local>
2022-12-15 09:28:34 +0800

    changes for v3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn jk React
         </a>
       </header>
     </div>
